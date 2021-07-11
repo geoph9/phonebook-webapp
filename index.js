54f6137ce2680bcb8afd4fc4fee5b212c11142e4
geoph9 <geoph9@protonmail.com>
2021-07-11 18:46:00 +0300

    up to part3.9 (step 9)

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
+app.use(morgan((tokens, req, res) => {
+  let out = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ]
+  if (tokens.method(req, res) === "POST") {
+  	//console.log(JSON.stringify(req.body))
+  	out = out.concat(JSON.stringify(req.body))
+  }
+  return out.join(' ')
+}))
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.get('/', (request, response) => {
+  response.send('<h1>Phonebook App!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  let html = `
+    <p>Phonebook has info for ${persons.length} people.</p>
+    <p>${new Date()}</p>`
+  response.send(html);
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+  	response.json(person)
+  } else {
+  	response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'Missing name.' 
+    })  	
+  }
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'Missing phone number.' 
+    })  	
+  }
+  const nameExists = persons.filter(p => p.name === body.name).length > 0
+  if (nameExists) {
+    return response.status(400).json({ 
+      error: 'Name already exists in the database.'
+    })  	
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
